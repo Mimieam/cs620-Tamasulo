0f6fd9c18ff7fcaf6e1c0ddcded2ba0558eb25c7
A. Miezan Echis <miezan@echimane.com>
2013-11-25 17:30:25 -0600

    FUnit and RStation

---
+++ Tamasulo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>    
+#include <string>
+#include <queue>          
+
+using namespace std;
+
+//Reservation Station
+class RStation {
+private:
+  /* data */
+public:
+  RStation(/* args */) = default;
+};
+
+
+// Functional Units
+class FUnit {
+private:
+  /* data */
+public:
+  FUnit(/* args */) = default;
+};
+
+int main(int argc, char const *argv[])
+{
+  
+  return 0;
+}
