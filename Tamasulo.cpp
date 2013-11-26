4f546d49faf0f976010d34b70385a4ffc6dbeefc
A. Miezan Echis <miezan@echimane.com>
2013-11-25 19:12:50 -0600

    Can Parse input

--- Tamasulo.cpp
+++ Tamasulo.cpp
@@ -1,22 +1,39 @@
-#include <iostream>    
+#include <iostream>
+#include <sstream>    
+#include <fstream>    
 #include <string>
-#include <queue>          
+#include <queue> 
+    
 
 using namespace std;
 
+
+
 class Instruction {
 private:
-  
+  string opCode;
+  string rs;
+  string rt;
+  string rd;
+  int imm;   // special I-type inst.
+  int Offset;// special J-type inst.
 public:
-  Instruction(/* args */) = default;
+  Instruction(){
+    
+  };
 };
+
+Instruction InstructionParser(vector<string> v) {
+
+
+}
 
 //Floating point register
 class FPRegisters {
 private:
   /* data */
 public:
-  FPRegisters(/* args */) = default;
+  FPRegisters(/* args */);
 };
 
 //Reservation Station
@@ -33,11 +50,37 @@ class FUnit {
 private:
   /* data */
 public:
-  FUnit(/* args */) = default;
+  FUnit(/* args */);
 };
 
 int main(int argc, char const *argv[])
 {
+  vector<string> vect;
+  string anInstr;
+
+  ifstream instFile ("data.txt");
+  if (instFile.is_open())
+    while ( getline (instFile, anInstr) )
+    {
+        cout << anInstr << endl;
+        string tok;
+        stringstream ss(anInstr);
+        while (ss >> tok)
+        {
+            vect.push_back(tok);
+          if (ss.peek() == ' ')
+            ss.ignore();
+          else{
+          }
+        }
+
+         for (int i=0; i<vect.size();i++){
+        cout << vect[i] << endl;
+      }
+        vect.clear();
+    }
+  else cout << "Unable to open file"; 
   
+  instFile.close();
   return 0;
 }
